3ccba6713a102c81e73f4246b9fb447405038051
蜂谷栗栖 <kurisu@blekksprut.net>
2024-12-27 19:49:16 +0900

    cli: flag to show entire date

--- cmd/gengou/main.go
+++ cmd/gengou/main.go
@@ -9,40 +9,42 @@ import (
 	"time"
 )
 
+type datefunc func(time.Time) string
+
 func main() {
-	widen := flag.Bool("w", false, "full-width")
-	version := flag.Bool("v", false, "version")
-	layout := flag.String("f", "2006.01.02", "time format")
+	w := flag.Bool("w", false, "full-width")
+	d := flag.Bool("d", false, "show full date")
+	v := flag.Bool("v", false, "version")
+	l := flag.String("f", "2006.01.02", "time format")
 
 	flag.Parse()
 
-	if *version {
+	if *v {
 		fmt.Printf("%s %s\n", os.Args[0], gengou.Version)
 		os.Exit(0)
 	}
 
-	if flag.NArg() < 1 {
-		now := time.Now()
-		eraYear := gengou.EraYear(now)
-		if *widen {
-			fmt.Println(width.Widen.String(eraYear))
-		} else {
-			fmt.Println(eraYear)
-		}
-	} else {
-		for _, arg := range flag.Args() {
-			date, err := time.Parse(*layout, arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "failed to parse date: %s\n", arg)
-				continue
-			}
-			eraYear := gengou.EraYear(date)
-			if *widen {
-				fmt.Println(width.Widen.String(eraYear))
-			} else {
-				fmt.Println(eraYear)
-			}
-		}
+	fun := gengou.EraYear
+	if *d {
+		fun = gengou.EraDate
 	}
 
+	dates := flag.Args()
+	if len(dates) < 1 {
+		now := time.Now()
+		dates = append(dates, now.Format(*l))
+	}
+
+	for _, raw := range dates {
+		date, err := time.Parse(*l, raw)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to parse date: %s\n", raw)
+			continue
+		}
+		if *w {
+			fmt.Println(width.Widen.String(fun(date)))
+		} else {
+			fmt.Println(fun(date))
+		}
+	}
 }
